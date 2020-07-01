0c2e47b9bab3ae9c676a6bebbfc05e75ac8434c6
ahmedhosny <ah2612650@gmail.com>
2020-07-01 10:00:54 +0200

    aasinish

---
+++ jsss.js
@@ -0,0 +1,386 @@
+	var a = document.getElementById("sw1");
+				a.onmouseover = function aaa(){
+					"use strict";
+					a.style.opacity="60%";
+					document.getElementById("down").style.display="block";
+					document.getElementById("pla").style.display="block";
+					
+				}
+				
+		var a = document.getElementById("sw1");
+			a.onmouseout = function aaa1(){
+					"use strict";
+					a.style.opacity="100%";
+					document.getElementById("down").style.display="none";
+					document.getElementById("pla").style.display="none";
+					onmouseover
+				}
+			
+		var b = document.getElementById("sw2");
+			b.onmouseover = function bbb(){
+					"use strict";
+					b.style.opacity="60%";
+					document.getElementById("down1").style.display="block";
+					document.getElementById("pla1").style.display="block";
+					
+				}
+		var b = document.getElementById("sw2");
+			b.onmouseout = function bbbbbbb1(){
+					"use strict";
+					b.style.opacity="100%";
+					document.getElementById("down1").style.display="none";
+					document.getElementById("pla1").style.display="none";
+					
+				}
+			var c = document.getElementById("sw3");
+			c.onmouseover = function ccc(){
+					"use strict";
+					c.style.opacity="60%";
+					document.getElementById("down2").style.display="block";
+					document.getElementById("pla2").style.display="block";
+					
+				}
+		var c = document.getElementById("sw3");
+			c.onmouseout = function cccc1(){
+					"use strict";
+					c.style.opacity="100%";
+					document.getElementById("down2").style.display="none";
+					document.getElementById("pla2").style.display="none";
+					
+				}
+		var d = document.getElementById("sw4");
+			d.onmouseover = function dddd(){
+					"use strict";
+					d.style.opacity="60%";
+					document.getElementById("down3").style.display="block";
+					document.getElementById("pla3").style.display="block";
+					
+				}
+		var d = document.getElementById("sw4");
+			d.onmouseout = function ddd(){
+					"use strict";
+					d.style.opacity="100%";
+					document.getElementById("down3").style.display="none";
+					document.getElementById("pla3").style.display="none";
+					
+				}
+		var e = document.getElementById("sw5");
+			e.onmouseover = function eeee(){
+					"use strict";
+					e.style.opacity="60%";
+					document.getElementById("down4").style.display="block";
+					document.getElementById("pla4").style.display="block";
+					
+				}
+		var e = document.getElementById("sw5");
+			e.onmouseout = function eee(){
+					"use strict";
+					e.style.opacity="100%";
+					document.getElementById("down4").style.display="none";
+					document.getElementById("pla4").style.display="none";
+					
+				}
+		var f = document.getElementById("sw6");
+			f.onmouseover = function fff(){
+					"use strict";
+					f.style.opacity="60%";
+					document.getElementById("down5").style.display="block";
+					document.getElementById("pla5").style.display="block";
+					
+				}
+		var f = document.getElementById("sw6");
+			f.onmouseout = function fff(){
+					"use strict";
+					f.style.opacity="100%";
+					document.getElementById("down5").style.display="none";
+					document.getElementById("pla5").style.display="none";
+					
+				}
+		var g = document.getElementById("sw7");
+			g.onmouseover = function ggg(){
+					"use strict";
+					g.style.opacity="60%";
+					document.getElementById("down6").style.display="block";
+					document.getElementById("pla6").style.display="block";
+					
+				}
+		var g = document.getElementById("sw7");
+			g.onmouseout = function gggg1(){
+					"use strict";
+					g.style.opacity="100%";
+					document.getElementById("down6").style.display="none";
+					document.getElementById("pla6").style.display="none";
+					
+				}
+		var i = document.getElementById("sw8");
+			i.onmouseover = function iii(){
+					"use strict";
+					i.style.opacity="60%";
+					document.getElementById("down7").style.display="block";
+					document.getElementById("pla7").style.display="block";
+					
+				}
+		var i = document.getElementById("sw8");
+			i.onmouseout = function iii1(){
+					"use strict";
+					i.style.opacity="100%";
+					document.getElementById("down7").style.display="none";
+					document.getElementById("pla7").style.display="none";
+					
+				}
+		var j = document.getElementById("sw9");
+			j.onmouseover = function jjj(){
+					"use strict";
+					j.style.opacity="60%";
+					document.getElementById("down8").style.display="block";
+					document.getElementById("pla8").style.display="block";
+					
+				}
+		var j = document.getElementById("sw9");
+			j.onmouseout = function jjjj(){
+					"use strict";
+					j.style.opacity="100%";
+					document.getElementById("down8").style.display="none";
+					document.getElementById("pla8").style.display="none";
+					
+				}
+		var k = document.getElementById("sw10");
+			k.onmouseover = function kkk(){
+					"use strict";
+					k.style.opacity="60%";
+					document.getElementById("down9").style.display="block";
+					document.getElementById("pla9").style.display="block";
+					
+				}
+		var k = document.getElementById("sw10");
+			k.onmouseout = function bbkb1(){
+					"use strict";
+					k.style.opacity="100%";
+					document.getElementById("down9").style.display="none";
+					document.getElementById("pla9").style.display="none";
+					
+				}
+		var l = document.getElementById("sw11");
+			l.onmouseover = function lll(){
+					"use strict";
+					l.style.opacity="60%";
+					document.getElementById("down10").style.display="block";
+					document.getElementById("pla10").style.display="block";
+					
+				}
+		var l = document.getElementById("sw11");
+			l.onmouseout = function lbbb1(){
+					"use strict";
+					l.style.opacity="100%";
+					document.getElementById("down10").style.display="none";
+					document.getElementById("pla10").style.display="none";
+					
+				}
+		var m = document.getElementById("sw12");
+			m.onmouseover = function mmmmb(){
+					"use strict";
+					m.style.opacity="60%";
+					document.getElementById("down11").style.display="block";
+					document.getElementById("pla11").style.display="block";
+					
+				}
+		var m = document.getElementById("sw12");
+			m.onmouseout = function bbmb1(){
+					"use strict";
+					m.style.opacity="100%";
+					document.getElementById("down11").style.display="none";
+					document.getElementById("pla11").style.display="none";
+					
+				}
+		var n = document.getElementById("sw13");
+			n.onmouseover = function bnb(){
+					"use strict";
+					n.style.opacity="60%";
+					document.getElementById("down12").style.display="block";
+					document.getElementById("pla12").style.display="block";
+					
+				}
+		var n = document.getElementById("sw13");
+			n.onmouseout = function bbnb1(){
+					"use strict";
+					n.style.opacity="100%";
+					document.getElementById("down12").style.display="none";
+					document.getElementById("pla12").style.display="none";
+					
+				}
+		var o = document.getElementById("sw14");
+			o.onmouseover = function bob(){
+					"use strict";
+					o.style.opacity="60%";
+					document.getElementById("down13").style.display="block";
+					document.getElementById("pla13").style.display="block";
+					
+				}
+		var o = document.getElementById("sw14");
+			o.onmouseout = function bobb1(){
+					"use strict";
+					o.style.opacity="100%";
+					document.getElementById("down13").style.display="none";
+					document.getElementById("pla13").style.display="none";
+					
+				}
+		var p = document.getElementById("sw15");
+			p.onmouseover = function bpb(){
+					"use strict";
+					p.style.opacity="60%";
+					document.getElementById("down14").style.display="block";
+					document.getElementById("pla14").style.display="block";
+					
+				}
+		var p = document.getElementById("sw15");
+			p.onmouseout = function bpbb1(){
+					"use strict";
+					p.style.opacity="100%";
+					document.getElementById("down14").style.display="none";
+					document.getElementById("pla14").style.display="none";
+					
+				}
+		var q = document.getElementById("sw16");
+			q.onmouseover = function bqb(){
+					"use strict";
+					q.style.opacity="60%";
+					document.getElementById("down15").style.display="block";
+					document.getElementById("pla15").style.display="block";
+					
+				}
+		var q = document.getElementById("sw16");
+			q.onmouseout = function bbqb1(){
+					"use strict";
+					q.style.opacity="100%";
+					document.getElementById("down15").style.display="none";
+					document.getElementById("pla15").style.display="none";
+					
+				}
+		var r = document.getElementById("sw17");
+			r.onmouseover = function brbb(){
+					"use strict";
+					r.style.opacity="60%";
+					document.getElementById("down16").style.display="block";
+					document.getElementById("pla16").style.display="block";
+					
+				}
+		var r = document.getElementById("sw17");
+			r.onmouseout = function bbrb1(){
+					"use strict";
+					r.style.opacity="100%";
+					document.getElementById("down16").style.display="none";
+					document.getElementById("pla16").style.display="none";
+					
+				}
+		var t = document.getElementById("sw18");
+			t.onmouseover = function bbtb(){
+					"use strict";
+					t.style.opacity="60%";
+					document.getElementById("down17").style.display="block";
+					document.getElementById("pla17").style.display="block";
+					
+				}
+		var t = document.getElementById("sw18");
+			t.onmouseout = function bbbbt1(){
+					"use strict";
+					t.style.opacity="100%";
+					document.getElementById("down17").style.display="none";
+					document.getElementById("pla17").style.display="none";
+					
+				}
+		var u = document.getElementById("sw19");
+			u.onmouseover = function bubb(){
+					"use strict";
+					u.style.opacity="60%";
+					document.getElementById("down18").style.display="block";
+					document.getElementById("pla18").style.display="block";
+					
+				}
+		var u = document.getElementById("sw19");
+			u.onmouseout = function bbbub1(){
+					"use strict";
+					u.style.opacity="100%";
+					document.getElementById("down18").style.display="none";
+					document.getElementById("pla18").style.display="none";
+					
+				}
+		var v = document.getElementById("sw20");
+			v.onmouseover = function bvbb(){
+					"use strict";
+					v.style.opacity="60%";
+					document.getElementById("down19").style.display="block";
+					document.getElementById("pla19").style.display="block";
+					
+				}
+		var v = document.getElementById("sw20");
+			v.onmouseout = function bvvb1(){
+					"use strict";
+					v.style.opacity="100%";
+					document.getElementById("down19").style.display="none";
+					document.getElementById("pla19").style.display="none";
+					
+				}
+		var w = document.getElementById("sw21");
+			w.onmouseover = function bwbb(){
+					"use strict";
+					w.style.opacity="60%";
+					document.getElementById("down20").style.display="block";
+					document.getElementById("pla20").style.display="block";
+					
+				}
+		var w = document.getElementById("sw21");
+			w.onmouseout = function bwbbb1(){
+					"use strict";
+					w.style.opacity="100%";
+					document.getElementById("down20").style.display="none";
+					document.getElementById("pla20").style.display="none";
+					
+				}
+		var x = document.getElementById("sw22");
+			x.onmouseover = function bbxb(){
+					"use strict";
+					x.style.opacity="60%";
+					document.getElementById("down21").style.display="block";
+					document.getElementById("pla21").style.display="block";
+					
+				}
+		var x = document.getElementById("sw22");
+			x.onmouseout = function bxbbb1(){
+					"use strict";
+					x.style.opacity="100%";
+					document.getElementById("down21").style.display="none";
+					document.getElementById("pla21").style.display="none";
+					
+				}
+		var y = document.getElementById("sw23");
+			y.onmouseover = function bybb(){
+					"use strict";
+					y.style.opacity="60%";
+					document.getElementById("down22").style.display="block";
+					document.getElementById("pla22").style.display="block";
+					
+				}
+		var y = document.getElementById("sw23");
+			y.onmouseout = function bybbb1(){
+					"use strict";
+					y.style.opacity="100%";
+					document.getElementById("down22").style.display="none";
+					document.getElementById("pla22").style.display="none";
+					
+				}
+		var z = document.getElementById("sw24");
+			z.onmouseover = function bzbb(){
+					"use strict";
+					z.style.opacity="60%";
+					document.getElementById("down23").style.display="block";
+					document.getElementById("pla23").style.display="block";
+					
+				}
+		var z = document.getElementById("sw24");
+			z.onmouseout = function bbzbb1(){
+					"use strict";
+					z.style.opacity="100%";
+					document.getElementById("down23").style.display="none";
+					document.getElementById("pla23").style.display="none";
+					
+				}
